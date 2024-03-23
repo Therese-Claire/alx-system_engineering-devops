1a07b39ae7ad9c602b67652f7165ab4dd573f98d
Therese-Claire <awonguagabi@gmail.com>
2024-03-22 18:13:34 -0700

    puppet script to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,10 @@
 # puppet declarative script to install flask from pip3.
-exec { 'install pip3':
-  command => 'apt-get install -y python3-pip',
-  unless  => 'which pip3',
-  path    => ['/bin', '/usr/bin'],
+exec { 'python3-pip':
+  ensure => installed,
 }
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Exec['install pip3']
+  require  => Package['python3-pip'],
 }
