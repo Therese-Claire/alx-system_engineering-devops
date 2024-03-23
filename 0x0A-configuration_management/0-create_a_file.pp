6188d889c9fbe8dfdff9906c3256d7d6b9aefb4e
Therese-Claire <awonguagabi@gmail.com>
2024-03-22 17:41:10 -0700

    puppet manifest to create new file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Creates a new file with content
-
+# manifest to create file in tmp directory
 file { '/tmp/school':
-  ensure => present,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
