6aa14064d55dc8cb82ca3a52b3a631d3196cbe76
Therese-Claire <awonguagabi@gmail.com>
2024-03-22 17:58:16 -0700

    puppet script to install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# manifest that installs flask from pip3
+# puppet declarative script to install flask from pip3.
 package { 'flask':
-  ensure => '2.1.0',
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
